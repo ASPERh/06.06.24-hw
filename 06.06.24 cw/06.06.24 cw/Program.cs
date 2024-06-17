842337e402fdb47e1b25d006b5fb673a4e8a6597
ASPER <144143767+ASPERh@users.noreply.github.com>
2024-06-17 20:28:19 +0100

    Add files via upload

---
+++ 06.06.24 cw/06.06.24 cw/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Имя: Дима");
+Console.WriteLine("Фамилия: Шевелёв");
+Console.WriteLine("Отчество: Алексеевич");
+Console.WriteLine("Учебное заведение: ITstep");
+Console.WriteLine("Группа: П26");
+Console.WriteLine("Возраст: 16");
+Console.WriteLine("Год рождения: 2007");
+Console.WriteLine("Год обучения: 3");
+Console.WriteLine("Работа: нет");
+Console.WriteLine("Хобби: готовка");
